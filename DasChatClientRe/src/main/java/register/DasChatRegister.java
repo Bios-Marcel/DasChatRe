a52c108f0e7074e729d90b2bde6d61fecda2e416
Bios-Marcel <marceloschr@googlemail.com>
2018-06-21 23:09:58 +0200

    Add Gradle and restructure project

---
+++ DasChatClientRe/src/main/java/register/DasChatRegister.java
@@ -0,0 +1,56 @@
+package register;
+
+import java.util.logging.Level;
+
+import controller.RegisterController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import logger.DasChatLogger;
+import login.DasChatClient;
+import util.DasChatUtil;
+
+public class DasChatRegister
+{
+	public DasChatRegister()
+	{
+		buildRegisterDialog();
+	}
+
+	private void buildRegisterDialog()
+	{
+		final Stage stage = new Stage();
+		final FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/RegisterDialog.fxml"));
+		loader.setController(new RegisterController(stage));
+		try
+		{
+			stage.setTitle("DasChat - Account erstellen");
+			stage.initOwner(DasChatClient.mainStage);
+			stage.initModality(Modality.WINDOW_MODAL);
+
+			final Parent root = loader.load();
+			final Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.getScene().getStylesheets().add(getClass().getResource("/style/application.css").toExternalForm());
+			stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/images/icon.png")));
+			stage.show();
+			stage.setIconified(false);
+			stage.setMaximized(false);
+			stage.setMinWidth(stage.getWidth());
+			stage.setMinHeight(stage.getHeight());
+			stage.setMaxWidth(stage.getWidth());
+			stage.setMaxHeight(stage.getHeight());
+		}
+		catch (Exception e)
+		{
+			DasChatLogger.getLogger().info("DasChat - Account erstellen konnte nicht geladen werden.");
+			DasChatLogger.getLogger().log(Level.SEVERE, e.getMessage(), e);
+			DasChatUtil.exit();
+		}
+	}
+
+}
